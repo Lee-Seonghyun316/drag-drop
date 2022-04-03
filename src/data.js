3abbe3773d20da3836b06e17f3bb87b05ac8dac8
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-04-04 00:45:46 +0900

    Data: 더미 데이터 변경

--- src/data.js
+++ src/data.js
@@ -5,8 +5,24 @@ export const dataBlocks = [
   { id: uuid(), name: 'Smarter alone, Smartest togetherI' },
   { id: uuid(), name: 'Make AI work for the rest of us' },
 ];
-export const cards = [
-  { id: uuid(), name: '데이터 슬롯' },
-  { id: uuid(), name: '함수 슬롯' },
-  { id: uuid(), name: '결과 슬롯' },
+
+export const functionBlocks = [
+  { id: uuid(), name: 'toUpperCase' },
+  { id: uuid(), name: 'wordNum' },
+  { id: uuid(), name: 'reverse' },
 ];
+
+export const dropRegionData = {
+  dataSlot: {
+    name: '데이터 슬롯',
+    items: [],
+  },
+  functionSlot: {
+    name: '함수 슬롯',
+    items: [],
+  },
+  resultSlot: {
+    name: '결과 슬롯',
+    items: [],
+  },
+};
